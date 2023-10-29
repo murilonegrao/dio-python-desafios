2ae850cb229935011b9417a818e3e56701ea1fac
Murilo Júlio da Silveira Negrão <murilonegrao@gmail.com>
2023-10-29 13:59:51 -0300

    Removidos arquivos não rastreados

--- banco_de_dados/engine.py
+++
@@ -1,5 +0,0 @@
-from sqlalchemy import create_engine
-
-
-URL_DB = 'postgresql://postgres:postgres@loccalhost:5432/desafio_dio_db'
-engine = create_engine(URL_DB, echo=True)
--- banco_de_dados_sql.py
+++
@@ -1,9 +0,0 @@
-import sqlalchemy
-import os
-
-POSTGRES_HOST = os.getenv('POSTGRES_HOST')
-POSTGRES_PORT = os.getenv('POSTGRES_PORT')
-POSTGRES_DB = os.getenv('POSTGRES_DB')
-POSTGRES_USER = os.getenv('POSTGRES_USER')
-POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
-
